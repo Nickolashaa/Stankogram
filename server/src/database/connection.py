0b33e41048ab4abe0a918406fe5beecc0f94de21
Nickolashaa <gracevnikolaj220@gmail.com>
2026-08-01 22:30:53 +0300

    server:move timestamp mixin in Model base

--- server/src/database/connection.py
+++ server/src/database/connection.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+
+from sqlalchemy import DateTime, func
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 from ..config import DATABASE_URL
 
@@ -8,4 +11,16 @@ session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
 class Base(DeclarativeBase):
-    pass
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
+        autoincrement=True,
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.current_timestamp(),
+    )
--- server/src/database/models/users.py
+++ server/src/database/models/users.py
@@ -4,7 +4,6 @@ from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.sql import expression
 
 from ..connection import Base
-from ..mixins.timestamp import TimestampMixin
 
 
 class UserType(Enum):
@@ -12,13 +11,9 @@ class UserType(Enum):
     TEACHER = "TEACHER"
 
 
-class User(Base, TimestampMixin):
+class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(
-        primary_key=True,
-        autoincrement=True,
-    )
     name: Mapped[str]
     surname: Mapped[str]
     patronymic: Mapped[str | None]
