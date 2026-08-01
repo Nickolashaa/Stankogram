0364143b9f76faffecf505e84a9ad4058506c302
Nickolashaa <gracevnikolaj220@gmail.com>
2026-08-02 01:20:24 +0300

    server:add NAMING_CONVENTION

--- server/src/database/connection.py
+++ server/src/database/connection.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import DateTime, func
+from sqlalchemy import DateTime, MetaData, func
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
@@ -9,8 +9,15 @@ from ..config import DATABASE_URL
 engine = create_async_engine(DATABASE_URL)
 session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
+NAMING_CONVENTION = {
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+}
+
 
 class Base(DeclarativeBase):
+    metadata = MetaData(naming_convention=NAMING_CONVENTION)
+
     id: Mapped[int] = mapped_column(
         primary_key=True,
         autoincrement=True,
