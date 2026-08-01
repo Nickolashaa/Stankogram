ad8fe7cabe325d3916a9f162fd5a2003e11efa05
Nickolashaa <gracevnikolaj220@gmail.com>
2026-08-02 02:37:12 +0300

    server:update session in di

--- server/src/dependencies/database.py
+++ server/src/dependencies/database.py
@@ -6,5 +6,12 @@ from ..database.connection import session_maker
 
 
 async def get_session() -> AsyncGenerator[AsyncSession]:
-    async with session_maker() as session:
+    session = session_maker()
+    try:
         yield session
+        await session.commit()
+    except Exception:
+        await session.rollback()
+        raise
+    finally:
+        await session.close()
