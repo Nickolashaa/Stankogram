3a5f7252a917eb6512193269123d05a1d3f8b211
Nickolashaa <gracevnikolaj220@gmail.com>
2026-08-03 07:29:32 +0300

    server:add unique

--- server/src/database/models/cancelled_jwt_tokens.py
+++ server/src/database/models/cancelled_jwt_tokens.py
@@ -1,6 +1,6 @@
 from uuid import UUID
 
-from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import Mapped, mapped_column
 
 from ..connection import Base
 
@@ -8,4 +8,4 @@ from ..connection import Base
 class CancelledRefreshToken(Base):
     __tablename__ = "cancelled_refresh_tokens"
 
-    jti: Mapped[UUID]
+    jti: Mapped[UUID] = mapped_column(unique=True)
