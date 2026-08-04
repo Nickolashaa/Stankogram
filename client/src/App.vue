b43b10cc5f5ee829485a8668d070596756fa5639
Nickolashaa <gracevnikolaj220@gmail.com>
2026-08-04 04:16:10 +0300

    client:update component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,22 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import { client } from "./api";
+import type { components } from "./api/schema";
+
+const data_var = ref<components["schemas"]["HealthResponse"]>();
 
 onMounted(async () => {
-  const { data, error } = await client.GET("/api/health");
+  const { data } = await client.GET("/api/health");
+  data_var.value = data;
 })
 </script>
 
 <template>
-  <h1>Stankogram</h1>
-  {{ data }}
-  {{ error }}
+  <template v-if="data_var === undefined"">Loading...</template>
+  <template v-else>
+    <h1>Stankogram</h1>
+    <h2>Code: {{ data_var?.code }}</h2>
+    <h3>Message: {{ data_var?.message }}</h3>
+  </template>
 </template>
-
 <style scoped></style>
