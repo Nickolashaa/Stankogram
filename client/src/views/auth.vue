8c567f372773c97f5ed905683fe040a20c5dabdc
Nickolashaa <gracevnikolaj220@gmail.com>
2026-08-09 18:11:34 +0300

    client:update auth text

--- client/src/views/auth.vue
+++ client/src/views/auth.vue
@@ -34,7 +34,7 @@ async function handleSubmit() {
     >
       <div class="flex flex-col gap-1.5 text-center">
         <h1 class="m-0 text-2xl font-semibold text-main">Stankogram</h1>
-        <h2 class="m-0 text-sm font-normal text-second">Университетский мессенджер</h2>
+        <h2 class="m-0 text-sm font-normal text-second">Мессенджер МГТУ СТАНКИН</h2>
       </div>
 
       <form class="flex flex-col gap-4" @submit.prevent="handleSubmit">
