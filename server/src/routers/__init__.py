37aeedba3d2bfc3c845c3d6d63c8852168ddf890
Nickolashaa <gracevnikolaj220@gmail.com>
2026-08-11 00:32:35 +0300

    server:create BaseResponse

--- server/src/routers/__init__.py
+++ server/src/routers/__init__.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
 
-from ..schemas.base import HealthResponse
 from .auth import router as auth_router
 from .users import router as users_router
 
@@ -9,6 +8,9 @@ router.include_router(users_router)
 router.include_router(auth_router)
 
 
-@router.get("/health", response_model=HealthResponse)
-async def health() -> HealthResponse:
-    return HealthResponse(code=200, message="Welcome to Stankogram API")
+@router.get("/health", response_model=dict[str, str])
+async def health() -> dict[str, str]:
+    return {
+        "code": "200",
+        "message": "Welcome to Stankogram API",
+    }
