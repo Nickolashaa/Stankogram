351da0d487035811f5810022e609156bc5dfbe17
Nickolashaa <gracevnikolaj220@gmail.com>
2026-08-13 00:12:59 +0300

    server:add user_id in messages

--- server/src/database/models/messages.py
+++ server/src/database/models/messages.py
@@ -4,11 +4,13 @@ from sqlalchemy.orm import Mapped, mapped_column
 from ...enums.messages import MessageType
 from ..connection import Base
 from .chats import Chat
+from .auth import User
 
 
 class Message(Base):
     __tablename__ = "messages"
 
     chat_id: Mapped[int] = mapped_column(ForeignKey(Chat.id, ondelete="CASCADE"))
+    user_id: Mapped[int] = mapped_column(ForeignKey(User.id, ondelete="CASCADE"))
     encrypted_text: Mapped[str]
     type: Mapped[MessageType]
