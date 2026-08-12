8ba6f0a40f5947499b45937c22870c22b9973a3a
Nickolashaa <gracevnikolaj220@gmail.com>
2026-08-13 01:30:34 +0300

    server:include message router

--- server/src/routers/__init__.py
+++ server/src/routers/__init__.py
@@ -2,10 +2,12 @@ from fastapi import APIRouter
 
 from .auth import router as auth_router
 from .chats import router as chats_router
+from .messages import router as messages_router
 
 router = APIRouter(prefix="/api")
 router.include_router(auth_router)
 router.include_router(chats_router)
+router.include_router(messages_router)
 
 
 @router.get("/health", response_model=dict[str, str])
