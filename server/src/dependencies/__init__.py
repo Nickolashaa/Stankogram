228bb42d42220a960c112723540c8a3beeca0585
Nickolashaa <gracevnikolaj220@gmail.com>
2026-08-16 22:27:35 +0300

    server:create get_deferred_message_event_service

--- server/src/dependencies/__init__.py
+++ server/src/dependencies/__init__.py
@@ -1,9 +1,9 @@
 from .auth import get_auth_service, get_current_user, get_current_user_ws
 from .chats import get_chat_service
 from .database import get_session
+from .deferred_message_events import get_deferred_message_event_service
 from .messages import get_message_service
 from .websockets import get_websocket_connection_manager
-from .deferred_message_events import get_deferred_message_event_service
 
 __all__ = (
     "get_session",
