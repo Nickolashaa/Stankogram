ae5068e56a668093c5ebec680bcde60b7fbef4dc
Nickolashaa <gracevnikolaj220@gmail.com>
2026-08-16 06:15:00 +0300

    server:add method to_ws_exception to BaseException

--- server/src/services/exceptions.py
+++ server/src/services/exceptions.py
@@ -1,27 +1,32 @@
 from fastapi import HTTPException
 
+from ..schemas.websockets import WebSocketError
+
 
 class BaseException(Exception):
-    http_code: int
+    code: int
 
     def to_http_exception(self) -> HTTPException:
         return HTTPException(
-            status_code=self.http_code,
+            status_code=self.code,
             detail=str(self),
         )
 
+    def to_ws_exception(self) -> WebSocketError:
+        return WebSocketError(code=self.code, message=str(self))
+
 
 class ObjectAlreadyExists(BaseException):
-    http_code = 422
+    code = 422
 
 
 class ObjectNotFound(BaseException):
-    http_code = 404
+    code = 404
 
 
 class InvalidInput(BaseException):
-    http_code = 422
+    code = 422
 
 
 class Forbidden(BaseException):
-    http_code = 403
+    code = 403
