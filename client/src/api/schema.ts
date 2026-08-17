18f7ca26e04be7d988af340365005129a278a632
Nickolashaa <gracevnikolaj220@gmail.com>
2026-08-17 03:00:25 +0300

    client:codegen

--- client/src/api/schema.ts
+++ client/src/api/schema.ts
@@ -225,40 +225,6 @@ export interface paths {
     patch?: never
     trace?: never
   }
-  "/api/chats": {
-    parameters: {
-      query?: never
-      header?: never
-      path?: never
-      cookie?: never
-    }
-    /** Get List */
-    get: operations["get_list_api_chats_get"]
-    put?: never
-    post?: never
-    delete?: never
-    options?: never
-    head?: never
-    patch?: never
-    trace?: never
-  }
-  "/api/messages/create": {
-    parameters: {
-      query?: never
-      header?: never
-      path?: never
-      cookie?: never
-    }
-    get?: never
-    put?: never
-    /** Create */
-    post: operations["create_api_messages_create_post"]
-    delete?: never
-    options?: never
-    head?: never
-    patch?: never
-    trace?: never
-  }
   "/api/messages": {
     parameters: {
       query?: never
@@ -266,8 +232,8 @@ export interface paths {
       path?: never
       cookie?: never
     }
-    /** Get List */
-    get: operations["get_list_api_messages_get"]
+    /** Get Messages */
+    get: operations["get_messages_api_messages_get"]
     put?: never
     post?: never
     delete?: never
@@ -297,12 +263,6 @@ export interface paths {
 export type webhooks = Record<string, never>
 export interface components {
   schemas: {
-    /** ChatProfileResponse */
-    ChatProfileResponse: {
-      chat: components["schemas"]["ChatResponse"]
-      /** Title */
-      title: string
-    }
     /** ChatResponse */
     ChatResponse: {
       /** Id */
@@ -336,13 +296,20 @@ export interface components {
       /** Refresh Token */
       refresh_token: string
     }
-    /** MessageCreate */
-    MessageCreate: {
+    /** MessageListQuery */
+    MessageListQuery: {
+      /**
+       * Limit
+       * @default 10
+       */
+      limit: number | null
+      /**
+       * Offset
+       * @default 0
+       */
+      offset: number | null
       /** Chat Id */
-      chat_id: number
-      type: components["schemas"]["MessageType"]
-      /** Text */
-      text: string
+      chat_id?: number | null
     }
     /** MessageResponse */
     MessageResponse: {
@@ -863,7 +830,7 @@ export interface operations {
           [name: string]: unknown
         }
         content: {
-          "application/json": components["schemas"]["ChatProfileResponse"]
+          "application/json": components["schemas"]["ChatResponse"]
         }
       }
       /** @description Validation Error */
@@ -877,66 +844,11 @@ export interface operations {
       }
     }
   }
-  get_list_api_chats_get: {
+  get_messages_api_messages_get: {
     parameters: {
-      query?: never
-      header?: never
-      path?: never
-      cookie?: never
-    }
-    requestBody?: never
-    responses: {
-      /** @description Successful Response */
-      200: {
-        headers: {
-          [name: string]: unknown
-        }
-        content: {
-          "application/json": components["schemas"]["ChatProfileResponse"][]
-        }
-      }
-    }
-  }
-  create_api_messages_create_post: {
-    parameters: {
-      query?: never
-      header?: never
-      path?: never
-      cookie?: never
-    }
-    requestBody: {
-      content: {
-        "application/json": components["schemas"]["MessageCreate"]
-      }
-    }
-    responses: {
-      /** @description Successful Response */
-      200: {
-        headers: {
-          [name: string]: unknown
-        }
-        content: {
-          "application/json": components["schemas"]["MessageResponse"]
-        }
-      }
-      /** @description Validation Error */
-      422: {
-        headers: {
-          [name: string]: unknown
-        }
-        content: {
-          "application/json": components["schemas"]["HTTPValidationError"]
-        }
-      }
-    }
-  }
-  get_list_api_messages_get: {
-    parameters: {
-      query?: {
-        limit?: number | null
-        offset?: number | null
-        chat_id?: number | null
-        user_id?: number | null
+      query: {
+        query: components["schemas"]["MessageListQuery"]
+        chat_id: number
       }
       header?: never
       path?: never
