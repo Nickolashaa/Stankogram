e1fa05e0bc04c26cdde9c57b2511448644458c19
Nickolashaa <gracevnikolaj220@gmail.com>
2026-08-19 00:22:27 +0300

    server:create ChatProfile

--- server/src/schemas/chats.py
+++ server/src/schemas/chats.py
@@ -1,6 +1,13 @@
 from ..enums.chats import ChatType
-from .base import BaseResponse
+from .base import BaseResponse, Schema
+from .messages import MessageResponse
 
 
 class ChatResponse(BaseResponse):
     type: ChatType
+
+
+class ChatProfile(Schema):
+    chat: ChatResponse
+    last_message: MessageResponse | None
+    title: str
