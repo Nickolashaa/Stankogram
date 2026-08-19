9534bf1f64e59231f24c4a263243c321868ff8ac
Nickolashaa <gracevnikolaj220@gmail.com>
2026-08-20 02:06:51 +0300

    server:create PublicChatProfileResponse and ChatParticipantResponse

--- server/src/schemas/chats.py
+++ server/src/schemas/chats.py
@@ -7,7 +7,17 @@ class ChatResponse(BaseResponse):
     type: ChatType
 
 
+class PublicChatProfileResponse(BaseResponse):
+    title: str
+    chat_id: int
+
+
 class ChatProfile(Schema):
     chat: ChatResponse
     last_message: MessageResponse | None
     title: str
+
+
+class ChatParticipantResponse(BaseResponse):
+    chat_id: int
+    user_id: int
