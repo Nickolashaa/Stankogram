16db8442fbfb3a325263538b06b776ade0c38313
Nickolashaa <gracevnikolaj220@gmail.com>
2026-08-20 00:43:20 +0300

    server:remove messages router

--- server/src/routers/__init__.py
+++ server/src/routers/__init__.py
@@ -2,13 +2,11 @@ from fastapi import APIRouter
 
 from .auth import router as auth_router
 from .chats import router as chats_router
-from .messages import router as messages_router
 from .websockets import router as ws_router
 
 router = APIRouter(prefix="/api")
 router.include_router(auth_router)
 router.include_router(chats_router)
-router.include_router(messages_router)
 router.include_router(ws_router)
 
 
