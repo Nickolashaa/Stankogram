bdefe9b2e196a0ecb18c2da3dc31c11fab28ced7
Nickolashaa <gracevnikolaj220@gmail.com>
2026-08-21 00:30:01 +0300

    server:init app

--- server/src/server/__init__.py
+++ server/src/server/__init__.py
@@ -1,2 +1,8 @@
+import uvicorn
+
+from .app import app
+from .config import UVICORN_HOST, UVICORN_PORT
+
+
 def main() -> None:
-    print("Hello from server!")
+    uvicorn.run(app, host=UVICORN_HOST, port=UVICORN_PORT)
