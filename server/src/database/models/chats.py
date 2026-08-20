9dd0a904d7c5e3e0bb6dd3d3717b155101a160d9
Nickolashaa <gracevnikolaj220@gmail.com>
2026-08-20 16:34:13 +0300

    server:refactoring database, remove model profiles

--- server/src/database/models/chats.py
+++ server/src/database/models/chats.py
@@ -10,6 +10,7 @@ class Chat(Base):
     __tablename__ = "chats"
 
     type: Mapped[ChatType]
+    title: Mapped[str | None]
 
 
 class ChatParticipant(Base):
@@ -19,10 +20,3 @@ class ChatParticipant(Base):
     user_id: Mapped[int] = mapped_column(ForeignKey(User.id, ondelete="CASCADE"))
 
     __table_args__ = (UniqueConstraint("user_id", "chat_id", name="uq_user_chat"),)
-
-
-class PublicChatProfile(Base):
-    __tablename__ = "public_chat_profiles"
-
-    title: Mapped[str]
-    chat_id: Mapped[int] = mapped_column(ForeignKey(Chat.id, ondelete="CASCADE"))
