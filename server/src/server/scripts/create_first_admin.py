e85122022f8973c6716476d14711da0babb937ca
Nickolashaa <gracevnikolaj220@gmail.com>
2026-08-21 00:31:01 +0300

    server:init scripts

---
+++ server/src/server/scripts/create_first_admin.py
@@ -0,0 +1,32 @@
+import asyncio
+
+from sqlalchemy import select
+
+from ..database.connection import session_maker
+from ..database.models.auth import User
+from ..enums.users import Role
+from ..services.auth import AuthService
+
+
+async def main() -> None:
+    async with session_maker() as session:
+        existing = await session.execute(select(User).where(User.is_admin.is_(True)))
+        if existing.scalar_one_or_none() is not None:
+            print("Admin already exists, aborting.")
+            return
+
+        service = AuthService(session)
+        await service.create(
+            name=input("Name: "),
+            surname=input("Surname: "),
+            patronymic=input("Patronymic: "),
+            email=input("Email: "),
+            role=Role.STUDENT,
+            is_admin=True,
+        )
+        await session.commit()
+
+    print("Success, credentials sended in your email.")
+
+
+asyncio.run(main())
