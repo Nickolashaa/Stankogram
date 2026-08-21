3939fdeff1b6ccc57fe3a30a974076694cea7e39
Nickolashaa <gracevnikolaj220@gmail.com>
2026-08-21 09:13:34 +0300

    server:update script schemagen

--- server/src/server/scripts/schemagen.py
+++ server/src/server/scripts/schemagen.py
@@ -1,5 +1,6 @@
 from ..graphql.schema import schema
 
-if __name__ == "__main__":
+
+def main() -> None:
     with open("schema.graphql", "w", encoding="utf-8") as f:
         f.write(schema.as_str() + "\n")
