85fbe7f23896c38126f7499c572c6ddea82529bd
Nickolashaa <gracevnikolaj220@gmail.com>
2026-08-22 23:28:52 +0300

    server:move _get_count_stmt from util to base service

--- server/src/server/utils/stmt_modificators.py
+++
@@ -1,7 +0,0 @@
-from typing import Any
-
-from sqlalchemy import Select, func, select
-
-
-def _get_count_stmt(stmt: Select[tuple[Any]]) -> Select[tuple[int]]:
-    return select(func.count()).select_from(stmt.subquery())
