c5a6513e73c1849ff6b4441e3178e1a6b882349e
Nickolashaa <gracevnikolaj220@gmail.com>
2026-08-23 21:51:01 +0300

    server:use new context in permissions

--- server/src/server/graphql/permissions/auth.py
+++ server/src/server/graphql/permissions/auth.py
@@ -2,20 +2,20 @@ from typing import Any
 
 from strawberry.permission import BasePermission
 
-from ..context import AppInfo
+from ..context import AuthorizedAppInfo
 
 
 class IsAuthenticated(BasePermission):
     message = "User is not authenticated"
 
-    def has_permission(self, source: Any, info: AppInfo, **kwargs) -> bool:
+    def has_permission(self, source: Any, info: AuthorizedAppInfo, **kwargs) -> bool:
         return info.context.current_user is not None
 
 
 class IsAdmin(BasePermission):
     message = "User is not admin"
 
-    def has_permission(self, source: Any, info: AppInfo, **kwargs) -> bool:
+    def has_permission(self, source: Any, info: AuthorizedAppInfo, **kwargs) -> bool:
         return (
             info.context.current_user is not None
             and info.context.current_user.is_admin is True
