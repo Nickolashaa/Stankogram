2ac79211f9051411655b558ff5f8f3a230cb0a9f
Nickolashaa <gracevnikolaj220@gmail.com>
2026-08-24 04:42:49 +0300

    server:create Message type

---
+++ server/src/server/graphql/types/messages/__init__.py
@@ -0,0 +1,7 @@
+from .enums import EMessageType
+from .types import Message
+
+__all__ = (
+    "Message",
+    "EMessageType",
+)
--- server/src/server/graphql/types/messages/types.py
+++ server/src/server/graphql/types/messages/types.py
@@ -1,9 +1,12 @@
 import strawberry
+
+from ..auth import IUser
 from ..base import IBaseType
-
-
+from ..chats import IChat
+from .enums import EMessageType
 
 
 @strawberry.type
-class Message(IBaseType):
-    
+class Message(IBaseType, IUser, IChat):
+    text: str
+    type: EMessageType
